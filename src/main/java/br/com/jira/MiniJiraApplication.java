3ec071231412ea5c246f6c0ba9695de2385d4a6b
Guilherme Alvino <guilhermealvi9@gmail.com>
2024-12-23 21:46:09 -0300

    Arquivos nao versionados

---
+++ src/main/java/br/com/jira/MiniJiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.jira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniJiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniJiraApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/jira/config/PostGreDbConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.jira.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostGreDbConfiguration {
+
+}
